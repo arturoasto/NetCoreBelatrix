db41281c46088ccac4535638716fadac279d0cfd
Arturo <arturo.asto@hotmail.com>
2019-05-29 18:58:48 -0500

    update

---
+++ Belatrix.WebApi.Repository.Postgresql/Configurations/OrderConfig.cs
@@ -0,0 +1,17 @@
+﻿using Belatrix.WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Belatrix.WebApi.Repository.Postgresql.Configurations
+{
+    internal class OrderConfig : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.ToTable("order");
+        }
+    }
+}
